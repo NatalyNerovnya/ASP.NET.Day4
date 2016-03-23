e642d8e621df757853a940a4046dc34394874e62
Наташа Неровня <natanerovnja@gmail.com>
2016-03-23 16:30:04 +0300

    Merge sorting with interface and delegate

---
+++ MergeSorting.NUnitTests/SortingTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MergeSorting;
+
+namespace MergeSorting.NUnitTests
+{
+    [TestClass]
+    public class SortingTests
+    {
+        [TestMethod]
+        public void SortArray_AnyArray_SortedArrayAccordingToTheSum()
+        {
+            int[] array1 = new[] {3, 5, 7, 2, 7, 8};
+            int[] array2 = new[] {4};
+            int[] array3 = {0, -4, -6, 3};
+
+            int[] result1 = new[] {2, 3, 5, 7, 7, 8};
+            int[] result2 = new[] {4};
+            int[] result3 = {-6, -4, 0, 3};
+
+            Sorting.MergeSort(array1);
+            Sorting.MergeSort(array2);
+            Sorting.MergeSort(array3);
+
+            CollectionAssert.AreEqual(array1, result1);
+            CollectionAssert.AreEqual(array2, result2);
+            CollectionAssert.AreEqual(array3, result3);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentNullException))]
+        public void SortArray_Null_ArgumentNullException()
+        {
+            Sorting.MergeSort(null);
+        }
+    }
+}
+
+
+
---
+++ MergeSorting/Sorting.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSorting
+{
+    /// <summary>
+    /// Merge sorting of integer array
+    /// </summary>
+    public class Sorting
+    {
+        #region Public Methods
+        /// <summary>
+        /// Sort array (increase)
+        /// </summary>
+        /// <param name="array"></param>
+        public static void MergeSort(int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            MergeSort(array, 0, array.Length - 1, null);
+        }
+
+        /// <summary>
+        /// Sort array according to customer comparer
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="compare">Comparision</param>
+        public static void MergeSort(int[] array, Comparison<int> compare)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (compare == null)
+                MergeSort(array, 0, array.Length - 1, null);
+            else
+                MergeSort(array, 0, array.Length - 1, compare);
+        }
+
+        /// <summary>
+        /// Sort array according to customer comparer
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="compare">IComparer</param>
+        public static void MergeSort(int[] array, IComparer<int> compare)
+        {
+            if(array == null)
+                throw new ArgumentNullException();
+            if (compare == null)
+                MergeSort(array, 0, array.Length - 1, null);
+            else
+                MergeSort(array, 0, array.Length - 1, compare.Compare);
+        }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Sort array with Merge Sort
+        /// </summary>
+        /// <param name="array">Array</param>
+        /// <param name="first">Index of first element</param>
+        /// <param name="last">Index of last element</param>
+        /// <param name="compare">Comparison</param>
+        private static void MergeSort(int[] array, int first, int last, Comparison<int> compare)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (first == last)
+                return;
+            if (first > last || first < 0 || last < 0)
+                throw new ArgumentException();
+            if (compare == null)
+            {
+                compare = Comparer<int>.Default.Compare;
+            }
+
+            int middle = (last + first - 1) / 2;
+            MergeSort(array, first, middle, compare);
+            MergeSort(array, middle + 1, last, compare);
+            Merge(array, first, middle, last, compare);
+
+        }
+
+        /// <summary>
+        /// Sort array from first elemnt to last
+        /// </summary>
+        /// <param name="array">Array</param>
+        /// <param name="first">Index of first element</param>
+        /// <param name="middle">Index of middle element</param>
+        /// <param name="last">Index of last element</param>
+        /// <param name="compare">Comparison</param>
+        private static void Merge(int[] array, int first, int middle, int last, Comparison<int> compare)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (first > last || first < 0 || last < 0 || first > middle || middle < 0)
+                throw new ArgumentException();
+            if (compare == null)
+            {
+                compare = Comparer<int>.Default.Compare;
+            }
+
+            int[] mergedArray = new int[last - first + 1];
+            for (int i = first, j = middle + 1, k = 0; k < mergedArray.Length; k++)
+            {
+                if (i > middle || j > last)
+                    mergedArray[k] = i > middle ? array[j++] : array[i++];
+                else
+                    mergedArray[k] = compare(array[i], array[j]) < 0 ? array[i++] : array[j++];
+            }
+
+            Array.Copy(mergedArray, 0, array, first, mergedArray.Length);
+        }
+
+        #endregion
+    }
+}
