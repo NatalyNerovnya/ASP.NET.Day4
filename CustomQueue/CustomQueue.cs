7e28bf683b59ac84c03b432e41fc70b0b74ca655
Наташа Неровня <natanerovnja@gmail.com>
2016-03-24 17:35:34 +0300

    Add custom queue

---
+++ CustomQueue/CustomQueue.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomQueue
+{
+    /// <summary>
+    /// Custom queu
+    /// </summary>
+    /// <typeparam name="T">Any type</typeparam>
+    public class CustomQueue<T> : IEnumerable<T>
+    {
+        #region Fields
+        private T[] queue;
+        private int capacity, head, tail;
+        private static readonly int defaultCapacity = 8;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Special constructor
+        /// </summary>
+        public CustomQueue() : this(defaultCapacity) { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="n">The length of queue</param>
+        public CustomQueue(int n)
+        {
+            if (n > 0)
+            {
+                queue = new T[n];
+                capacity = n;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            head = 0;
+            tail = 0;
+        }
+
+        ///// <summary>
+        ///// Constructor
+        ///// </summary>
+        ///// <param name="arr">array of values</param>
+        //public CustomQueue(params T[] arr)
+        //    : this(arr.Length)
+        //{
+        //    Array.Copy(arr, queue, capacity);
+        //    tail = capacity - 1;
+        //}
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="arr">Array of elements</param>
+        public CustomQueue(IEnumerable<T> arr)
+    : this(arr.ToArray().Length)
+        {
+            if (arr == null)
+                return;
+            Array.Copy(arr.ToArray(), queue, capacity);
+            tail = capacity - 1;
+        }
+
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Add element to the tail of queue
+        /// </summary>
+        /// <param name="element">element</param>
+        public void Enqueue(T element)
+        {
+            if ((tail + 1) % capacity == head % capacity)
+            {
+                Extend();
+                queue[capacity] = element;
+                tail = capacity;
+                head = 0;
+                capacity *= 2;
+            }
+            else
+            {
+                queue[++tail % capacity] = element;
+                tail = tail % capacity;
+            }
+
+        }
+
+        /// <summary>
+        /// Return the element from the head of the queue and delete it 
+        /// </summary>
+        /// <returns>The first  element</returns>
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new ArgumentException();
+
+            T returnValue = Peek();
+            DeleteHead();
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Return first element
+        /// </summary>
+        /// <returns>First element</returns>
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new ArgumentException();
+            return queue[head];
+        }
+
+        /// <summary>
+        /// Delete all elements from the queue
+        /// </summary>
+        public void Clear()
+        {
+            for (int i = head, j = 0; j < capacity; j++)
+            {
+                //queue[i] = null;
+                i = ++i % capacity;
+            }
+            head = tail = 0;
+        }
+
+        /// <summary>
+        /// Does the queue contain the element?
+        /// </summary>
+        /// <param name="str">element</param>
+        /// <returns>True, if the queue contains the element</returns>
+        public bool Contain(T str)
+        {
+            foreach (var variable in queue)
+            {
+                if (variable.Equals(str))
+                    return true;
+            }
+            return false;
+        }
+
+        #endregion
+
+        #region Interface implementation
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new QueueIterator<T>(queue, tail, head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+        
+        #region Private Methods
+        /// <summary>
+        /// Extend capacity in 2 times
+        /// </summary>
+        private void Extend()
+        {
+            T[] wideQueue = new T[capacity * 2];
+
+            for (int i = head, j = 0; j < capacity; j++)
+            {
+                wideQueue[j] = queue[i];
+                i = ++i % capacity;
+            }
+            queue = wideQueue;
+        }
+
+        /// <summary>
+        /// Delete head
+        /// </summary>
+        private void DeleteHead()
+        {
+            //queue[head] = null;
+            head = ++head % capacity;
+        }
+
+        /// <summary>
+        /// Check weather the queue is empty
+        /// </summary>
+        /// <returns>True, if the queue ie empty</returns>
+        private bool IsEmpty()
+        {
+            return tail % capacity == head % capacity;
+        }
+
+        #endregion
+    }
+}
---
+++ CustomQueue/QueueIterator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomQueue
+{
+    /// <summary>
+    /// Custom iterator
+    /// </summary>
+    /// <typeparam name="T">Any type</typeparam>
+    class QueueIterator<T> : IEnumerator<T>
+    {
+        #region Fields
+        private T[] array;
+        private int position;
+        private readonly int tailPosition, headPosition;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="list">Array of elements</param>
+        /// <param name="tail">Index of last element</param>
+        /// <param name="head">Index of first element</param>
+        public QueueIterator(T[] list, int tail, int head)
+        {
+            array = list;
+            position = (head - 1) % list.Length;
+            tailPosition = tail;
+            headPosition = head;
+        }
+        #endregion
+
+        #region Implementation of IEnumerator<T> methods
+        /// <summary>
+        /// Go to the next position
+        /// </summary>
+        /// <returns>False, if collection is finished</returns>
+        public bool MoveNext()
+        {
+            if (position == tailPosition)
+            {
+                Reset();
+                return false;
+            }
+            position = ++position % array.Length;
+            return true;
+        }
+
+        /// <summary>
+        /// Set index to the pre-begin
+        /// </summary>
+        public void Reset()
+        {
+            position = (headPosition - 1) % array.Length;
+        }
+
+        /// <summary>
+        /// Return current element
+        /// </summary>
+        object IEnumerator.Current
+        {
+            get { return array[position]; }
+        }
+
+        public void Dispose()
+        {
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Return current element
+        /// </summary>
+        public T Current
+        {
+            get { return array[position]; }
+        }
+        #endregion
+    }
+}
