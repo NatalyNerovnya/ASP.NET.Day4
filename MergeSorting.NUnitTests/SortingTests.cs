6db6052c8520d0e8f410f557c9aeb25778fc052f
Наташа Неровня <natanerovnja@gmail.com>
2016-04-04 01:08:35 +0300

    Add NUnit tests for sorting

--- MergeSorting.NUnitTests/SortingTests.cs
+++ MergeSorting.NUnitTests/SortingTests.cs
@@ -8,33 +8,52 @@ namespace MergeSorting.NUnitTests
     [TestClass]
     public class SortingTests
     {
+        #region Test Method
         [TestMethod]
-        public void SortArray_AnyArray_SortedArrayAccordingToTheSum()
+        public void SortArray_AnyArray_SortedArrayAccordingToTheMax()
         {
-            int[] array1 = new[] {3, 5, 7, 2, 7, 8};
-            int[] array2 = new[] {4};
-            int[] array3 = {0, -4, -6, 3};
-
-            int[] result1 = new[] {2, 3, 5, 7, 7, 8};
-            int[] result2 = new[] {4};
-            int[] result3 = {-6, -4, 0, 3};
+            int[] array1 = new[] { 3, 5, 7, 2, 7, 8 };
+            int[] result1 = new[] { 2, 3, 5, 7, 7, 8 };
 
             Sorting.MergeSort(array1);
-            Sorting.MergeSort(array2);
-            Sorting.MergeSort(array3);
 
             CollectionAssert.AreEqual(array1, result1);
-            CollectionAssert.AreEqual(array2, result2);
-            CollectionAssert.AreEqual(array3, result3);
 
         }
 
         [TestMethod]
-        [ExpectedException(typeof (ArgumentNullException))]
+        public void SortArray_AnyArray_SortedArrayAccordingToTheMin()
+        {
+            int[] array1 = { 0, -4, -6, 3 };
+            int[] result1 = { 3, 0, -4, -6 };
+            Comparison<int> compare = CompareByMin;
+
+            Sorting.MergeSort(array1, compare);
+
+            CollectionAssert.AreEqual(array1, result1);
+        }
+        #endregion
+
+        #region Test exeption
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void SortArray_Null_ArgumentNullException()
         {
             Sorting.MergeSort(null);
         }
+        #endregion
+
+        #region Private methods
+        private int CompareByMin(int a, int b)
+        {
+            if (a < b)
+                return 1;
+            else if (a > b)
+                return -1;
+            else
+                return 0;
+        }
+        #endregion
     }
 }
 
